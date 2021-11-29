df5c75947557b2dc641399e3a71abc78a91d9727
Voldemar Lazarev <voldemar.lazarev@gmail.com>
2021-11-29 22:48:16 +0200

    Updated project files

--- Projects/AN3413-Current_consumption_touch_sensing/TrueSTUDIO/STM32L152RC(32L152C-Discovery)/Debug/CMSIS/subdir.mk
+++ Projects/AN3413-Current_consumption_touch_sensing/TrueSTUDIO/STM32L152RC(32L152C-Discovery)/Debug/CMSIS/subdir.mk
@@ -16,5 +16,12 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 CMSIS/system_stm32l1xx.o: H:/Documents/Embedded/STM32L-Discovery_FW_Pack_2/STM32L-Discovery_FW_Pack/Projects/AN3413-Current_consumption_touch_sensing/src/system_stm32l1xx.c CMSIS/subdir.mk
-	arm-none-eabi-gcc -c "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DUSE_STDPERIPH_DRIVER -DTSLPRM_STM32L1XX_SW_ACQ -DSTM32L1XX_MDP -c -I../../../inc -I../../../../../Libraries/STMTouch_Driver/inc -I../../../../../Libraries/CMSIS/Include -I../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc -I../../../../../Libraries/CMSIS/Device/ST/STM32L1xx/Include -I../../../../../Utilities/STM32L-DISCOVERY -Og -ffunction-sections -fdata-sections -Wall -mlong-calls -Wno-pointer-sign -Wno-missing-braces -Wformat=0 -fstack-usage -MMD -MP -MF"CMSIS/system_stm32l1xx.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc -c "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DUSE_STDPERIPH_DRIVER -DTSLPRM_STM32L1XX_SW_ACQ -DSTM32L1XX_MDP -c -I../../../inc -I../../../../../Libraries/STMTouch_Driver/inc -I../../../../../Libraries/CMSIS/Include -I../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc -I../../../../../Libraries/CMSIS/Device/ST/STM32L1xx/Include -I../../../../../Utilities/STM32L-DISCOVERY -O0 -ffunction-sections -fdata-sections -Wall -mlong-calls -Wno-pointer-sign -Wno-missing-braces -Wformat=0 -fstack-usage -MMD -MP -MF"CMSIS/system_stm32l1xx.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-CMSIS
+
+clean-CMSIS:
+	-$(RM) ./CMSIS/system_stm32l1xx.d ./CMSIS/system_stm32l1xx.o
+
+.PHONY: clean-CMSIS
 
