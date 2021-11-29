0b7d70350274f73cb14bfe2d5c26a45fca135b4d
Voldemar Lazarev <voldemar.lazarev@gmail.com>
2021-11-29 22:53:10 +0200

    Revert "partially updated other libraries"
    This reverts commit 01288d3713f90e7248bb97e6235c01b115d01e77.

---
+++ Projects/AN3413-Current_consumption_touch_sensing/TrueSTUDIO/STM32L152RC(32L152C-Discovery)/Debug/STM32L-Discovery/stm32l_discovery_lcd.d
@@ -0,0 +1,154 @@
+STM32L-Discovery/stm32l_discovery_lcd.o: \
+ H:/Documents/Embedded/STM32L-Discovery_FW_Pack_2/STM32L-Discovery_FW_Pack/Utilities/STM32L-DISCOVERY/stm32l_discovery_lcd.c \
+ H:/Documents/Embedded/STM32L-Discovery_FW_Pack_2/STM32L-Discovery_FW_Pack/Utilities/STM32L-DISCOVERY/stm32l_discovery_lcd.h \
+ ../../../../../Libraries/CMSIS/Device/ST/STM32L1xx/Include/stm32l1xx.h \
+ ../../../../../Libraries/CMSIS/Include/core_cm3.h \
+ ../../../../../Libraries/CMSIS/Include/core_cmInstr.h \
+ ../../../../../Libraries/CMSIS/Include/core_cmFunc.h \
+ ../../../../../Libraries/CMSIS/Device/ST/STM32L1xx/Include/system_stm32l1xx.h \
+ ../../../inc/stm32l1xx_conf.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_adc.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_crc.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_comp.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dac.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dbgmcu.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dma.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_exti.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_flash.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_gpio.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_syscfg.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_i2c.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_iwdg.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_lcd.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_pwr.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_rcc.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_rtc.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_spi.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_tim.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_usart.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_wwdg.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/misc.h \
+ H:/Documents/Embedded/STM32L-Discovery_FW_Pack_2/STM32L-Discovery_FW_Pack/Utilities/STM32L-DISCOVERY/discover_board.h \
+ ../../../inc/main.h \
+ ../../../../../Utilities/STM32L-DISCOVERY/discover_board.h \
+ ../../../../../Utilities/STM32L-DISCOVERY/stm32l_discovery_lcd.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_acq.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_acq_stm32l1xx_sw.h \
+ ../../../inc/tsl_conf_stm32l1xx.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_check_config.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_check_config_stm32l1xx.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_types.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_time.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_time_stm32l1xx.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_touchkey.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_linrot.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_object.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_dxs.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_ecs.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_filter.h \
+ ../../../../../Libraries/STMTouch_Driver/inc/tsl_globals.h \
+ ../../../inc/tsl_user.h ../../../inc/icc_measure.h \
+ ../../../inc/discover_functions.h
+
+H:/Documents/Embedded/STM32L-Discovery_FW_Pack_2/STM32L-Discovery_FW_Pack/Utilities/STM32L-DISCOVERY/stm32l_discovery_lcd.h:
+
+../../../../../Libraries/CMSIS/Device/ST/STM32L1xx/Include/stm32l1xx.h:
+
+../../../../../Libraries/CMSIS/Include/core_cm3.h:
+
+../../../../../Libraries/CMSIS/Include/core_cmInstr.h:
+
+../../../../../Libraries/CMSIS/Include/core_cmFunc.h:
+
+../../../../../Libraries/CMSIS/Device/ST/STM32L1xx/Include/system_stm32l1xx.h:
+
+../../../inc/stm32l1xx_conf.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_adc.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_crc.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_comp.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dac.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dbgmcu.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dma.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_exti.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_flash.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_gpio.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_syscfg.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_i2c.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_iwdg.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_lcd.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_pwr.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_rcc.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_rtc.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_spi.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_tim.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_usart.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_wwdg.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/misc.h:
+
+H:/Documents/Embedded/STM32L-Discovery_FW_Pack_2/STM32L-Discovery_FW_Pack/Utilities/STM32L-DISCOVERY/discover_board.h:
+
+../../../inc/main.h:
+
+../../../../../Utilities/STM32L-DISCOVERY/discover_board.h:
+
+../../../../../Utilities/STM32L-DISCOVERY/stm32l_discovery_lcd.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_acq.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_acq_stm32l1xx_sw.h:
+
+../../../inc/tsl_conf_stm32l1xx.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_check_config.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_check_config_stm32l1xx.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_types.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_time.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_time_stm32l1xx.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_touchkey.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_linrot.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_object.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_dxs.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_ecs.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_filter.h:
+
+../../../../../Libraries/STMTouch_Driver/inc/tsl_globals.h:
+
+../../../inc/tsl_user.h:
+
+../../../inc/icc_measure.h:
+
+../../../inc/discover_functions.h:
---
+++ Projects/AN3413-Current_consumption_touch_sensing/TrueSTUDIO/STM32L152RC(32L152C-Discovery)/Debug/StdPeriph_Driver/stm32l1xx_rtc.d
@@ -0,0 +1,83 @@
+StdPeriph_Driver/stm32l1xx_rtc.o: \
+ H:/Documents/Embedded/STM32L-Discovery_FW_Pack_2/STM32L-Discovery_FW_Pack/Libraries/STM32L1xx_StdPeriph_Driver/src/stm32l1xx_rtc.c \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_rtc.h \
+ ../../../../../Libraries/CMSIS/Device/ST/STM32L1xx/Include/stm32l1xx.h \
+ ../../../../../Libraries/CMSIS/Include/core_cm3.h \
+ ../../../../../Libraries/CMSIS/Include/core_cmInstr.h \
+ ../../../../../Libraries/CMSIS/Include/core_cmFunc.h \
+ ../../../../../Libraries/CMSIS/Device/ST/STM32L1xx/Include/system_stm32l1xx.h \
+ ../../../inc/stm32l1xx_conf.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_adc.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_crc.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_comp.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dac.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dbgmcu.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dma.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_exti.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_flash.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_gpio.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_syscfg.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_i2c.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_iwdg.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_lcd.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_pwr.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_rcc.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_spi.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_tim.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_usart.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_wwdg.h \
+ ../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/misc.h
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_rtc.h:
+
+../../../../../Libraries/CMSIS/Device/ST/STM32L1xx/Include/stm32l1xx.h:
+
+../../../../../Libraries/CMSIS/Include/core_cm3.h:
+
+../../../../../Libraries/CMSIS/Include/core_cmInstr.h:
+
+../../../../../Libraries/CMSIS/Include/core_cmFunc.h:
+
+../../../../../Libraries/CMSIS/Device/ST/STM32L1xx/Include/system_stm32l1xx.h:
+
+../../../inc/stm32l1xx_conf.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_adc.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_crc.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_comp.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dac.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dbgmcu.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dma.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_exti.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_flash.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_gpio.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_syscfg.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_i2c.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_iwdg.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_lcd.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_pwr.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_rcc.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_spi.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_tim.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_usart.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_wwdg.h:
+
+../../../../../Libraries/STM32L1xx_StdPeriph_Driver/inc/misc.h:
